6bf29dcebc604dd9b051731c26578a3ad1eacd5c
kalai <kalai@DESKTOP-SRCGU3R>
2022-12-23 20:00:45 +0530

    program

---
+++ src/com/javalearning/SumOfDigits.java
@@ -0,0 +1,46 @@
+package com.javalearning;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+	  // int num =543;
+	//   int sum =0;
+	//   int temp;
+	//   while(num>0) {
+	//	     temp=num %10;
+	//	     sum=sum+temp;
+	    
+	 //   	num =num / 10 ;
+	 //    }
+	 //        System.out.println(" sum of digits "  +sum);
+	//}
+//}  //another method
+   //  long num=3451;
+   //  long sum;
+   //  for(sum=0;num>0;num=num/10) {
+   // 	 sum=sum+num %10;
+   /////  }
+   //  System.out.println("sum of digits:"+sum);
+   //  }
+//}  // 1 to 10 (sum)
+   int sum=0;
+  // for(int i=1;i<=10;i++) {
+//	   sum=sum+i;
+  // }
+  //  System.out.println(sum);
+////	}
+//}
+int i=1;
+while(i<=10) {
+	sum=sum+i;
+	i++;
+}
+	System.out.println(sum);
+	
+	}
+}
+	
+	
+	
