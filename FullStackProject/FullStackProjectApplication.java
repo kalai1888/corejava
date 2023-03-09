95f19786453168a47a9955256c8a7ed44e125427
kalai1888 <120959320+kalai1888@users.noreply.github.com>
2023-03-09 13:47:20 +0530

    Add files via upload

---
+++ FullStackProject/FullStackProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.FullStackProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class FullStackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackProjectApplication.class, args);
+	}
+
+}
